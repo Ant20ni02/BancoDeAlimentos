fd780a998a0eda9aa3eb2310b81c47e23b389cd3
ant20ni02 <antoniogarcort@hotmail.com>
2022-09-27 19:25:10 -0500

    sign up functionality added, still missing things

--- web-app/src/pages/SignUp.js
+++ web-app/src/pages/SignUp.js
@@ -1,7 +1,10 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/form.css';
-import FormButton from '../components/FormButton';
+
+const options = [
+    'Masculino', 'Femenino'
+  ];
 
 function SignUp() {
     const form = useRef();
@@ -9,7 +12,12 @@ function SignUp() {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const firstName = form.current.firstName.value;
+        const lastName = form.current.lastName.value;
         const email = form.current.email.value;
+        const phoneNumber = form.current.phoneNumber.value;
+        const sex = form.current.sex.value;
         const password = form.current.password.value;
         const passwordConfirm = form.current.passwordConfirm.value;
 
@@ -18,7 +26,23 @@ function SignUp() {
         } else {
             if (password === passwordConfirm) {
                 navigate('/home', { replace: true });
+                let formData = JSON.stringify({ firstName: firstName, lastName: lastName, phoneNumber: phoneNumber, sex: sex, email: email, password_: password });
+                const response = await fetch('http://localhost:4000/login',{ method: 'POST',headers: {
+                'Content-Type': 'application/json',
+                }, body: formData});
+                const data = await response.json();
+                if(data.mensaje !== 'Usuario o contraseña inválidos'){
+                    localStorage.setItem('token', data.token);
+                    localStorage.setItem('idUser', data.idUser);
+                    console.log(data.token);
+                    console.log(data.idUser);
+                    navigate('/home', { replace: true });
+                }
+                else{
+                    alert('Error al registrarse');
+                }
             } else {
+                
                 alert('Error al registrarse');
             }
         }
@@ -27,7 +51,14 @@ function SignUp() {
         <>
             <h1 className="form">Registrarse</h1>
             <form ref={form} onSubmit={handleSubmit}>
-                <input type="email" name="email" placeholder="Correo electrónico" /* pattern="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?" */ required/>
+                <input name="firstName" placeholder="Nombres" required/>
+                <input name="lastName" placeholder="Apellidos" required/>
+                <input type="email" name="email" placeholder="Correo electrónico" required/>
+                <input name="phoneNumber" placeholder="Número de teléfono" pattern="[0-9]" required/>
+                <select name="sex">
+                    <option value="m">Masculino</option>
+                    <option value="f">Femenino</option>
+                </select>
                 <input type="password" name="password" placeholder="Contraseña" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" required/>
                 <input type="password" name="passwordConfirm" placeholder="Confirmar contraseña" pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$" required/>
                 <button type="submit">Continuar</button>
