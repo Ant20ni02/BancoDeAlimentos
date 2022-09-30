78e9e6165ecf9d5b4b3a095d5d6065408fd0fa5c
MikeDev0X <mike0vortex@gmail.com>
2022-09-30 17:34:43 -0500

    prototipo updated

--- Prototipo/app/src/main/java/mx/tec/prototipo/MainActivity.kt
+++ Prototipo/app/src/main/java/mx/tec/prototipo/MainActivity.kt
@@ -3,11 +3,31 @@ package mx.tec.prototipo
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
+
+    val usernameTv =findViewById<TextView>(R.id.txtUsername_Voluntario)
+    val passwordTv = findViewById<TextView>(R.id.txtPassword)
+
+    override fun getBody(): ByteArray {
+        val params2 = HashMap<String, String>()
+        params2.put("email", usernameTv.text.toString())
+        params2.put("password_", passwordTv.text.toString())
+
+        return JSONObject(params2).toString().toByteArray()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,6 +41,14 @@ class MainActivity : AppCompatActivity() {
 
         //Si Tiene Cuenta the Voluntario o Familia
         btnLogin.setOnClickListener{
+
+        val loginUrl = "http://localhost:4000/login"
+
+
+
+
+
+
             //Si es cuenta de Voluntario o Familia
             if(Username.text.toString() == "User" && Password.text.toString() == "User1234"
                 || Username.text.toString() == "Family" && Password.text.toString() == "Family1234"){
@@ -47,4 +75,38 @@ class MainActivity : AppCompatActivity() {
             System.exit(0)
         }
     }
+
+    fun sendcall(url:String){
+        //RequestQueue initialized
+        var mRequestQueue = Volley.newRequestQueue(this)
+
+        //String Request initialized
+        var mStringRequest = object : StringRequest(Request.Method.POST, url, Response.Listener { response ->
+            Toast.makeText(applicationContext, "Logged In Successfully", Toast.LENGTH_SHORT).show()
+
+
+        }, Response.ErrorListener { error ->
+            Log.i("This is the error", "Error :" + error.toString())
+            Toast.makeText(applicationContext, "Please make sure you enter correct password and username", Toast.LENGTH_SHORT).show()
+        }) {
+            override fun getBodyContentType(): String {
+                return "application/json"
+            }
+
+            @Throws(AuthFailureError::class)
+            override fun getBody(): ByteArray {
+                val params2 = HashMap<String, String>()
+                params2.put("Login","your credentials" )
+                params2.put("Password", "your credentials")
+                return JSONObject(params2).toString().toByteArray()
+            }
+
+        }
+        mRequestQueue!!.add(mStringRequest!!)
+
+
+    }
+
+
+
 }
