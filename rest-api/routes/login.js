7a5a18d8f87aa5e4eeb76e14f9e54002bdf59e5b
MikeDev0X <mike0vortex@gmail.com>
2022-10-04 17:27:46 -0500

    getusersdata fixed

--- rest-api/routes/login.js
+++ rest-api/routes/login.js
@@ -4,6 +4,6 @@ const loginController = require('../controller/loginController');
 //const middleware = require('../middleware/jwt-middleware');
 
 router.post('/login', loginController.login);
-router.post('/getUsersData', loginController.getUsersData);
+router.get('/getUsersData:idUser', loginController.getUsersData);
 
 module.exports = router;
