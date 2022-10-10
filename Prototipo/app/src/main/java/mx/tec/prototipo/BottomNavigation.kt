e199c158caf694df6fd2de9f2328953f39a258af
MikeDev0X <mike0vortex@gmail.com>
2022-10-10 11:46:53 -0500

    bnv fragment layouts added

--- Prototipo/app/src/main/java/mx/tec/prototipo/BottomNavigation.kt
+++ Prototipo/app/src/main/java/mx/tec/prototipo/BottomNavigation.kt
@@ -2,13 +2,36 @@ package mx.tec.prototipo
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class BottomNavigation: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_bottomnav)
 
+        val mainEncuestaFragment = MainEncuestaFragment()
+        val bottomNav =findViewById<BottomNavigationView>(R.id.bnv_main)
+
+        loadFragment(mainEncuestaFragment) //loads the button fragment for the first time
+
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.nav_menu ->{
+                    loadFragment(mainEncuestaFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+
     }
 
+    private  fun loadFragment(fragment: Fragment){
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.fragmentContainer,fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
 }
