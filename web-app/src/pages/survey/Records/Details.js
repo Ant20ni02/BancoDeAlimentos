4ff41acd682b650b55d7f274af371f78a9ea34a1
Ant20ni02 <antoniogarcort@hotmail.com>
2022-10-14 00:58:11 -0500

    surveyInformation added to details

--- web-app/src/pages/survey/Records/Details.js
+++ web-app/src/pages/survey/Records/Details.js
@@ -1,20 +1,42 @@
+import { useState, useEffect } from 'react';
 import TextQuestion from "../../../components/Records/TextQuestion";
 import CheckboxQuestion from "../../../components/Records/CheckboxQuestion";
 import {Link, useParams} from "react-router-dom";
-import LinkButton from "../../../components/LinkButton";
+//import LinkButton from "../../../components/LinkButton";
 import '../../../styles/Details.css';
+import url from '../../../config/API';
 import QuestionsText from "../../../data/Questions-text";
 import QuestionsCheckbox from "../../../data/Questions-checkbox";
 
 function Details() {
     const {idSurvey} = useParams();
-    console.log(idSurvey);
+    const [surveyData, setSurveyData] = useState({
+        idFamily: "No hay datos",
+        idSurvey: 0,
+        latitude: 0,
+        longitude: 0,
+        familyLastName: "No hay datos",
+        date_: "00-00-0000"
+    });
+
+    const getSurveyData = async (e) => {
+    
+        const response = await fetch(url+`getFamilyById/${idSurvey}`,{method: 'GET',
+                                headers: {'x-access-token' : localStorage.getItem('token')} });
+        const data = await response.json();
+        setSurveyData(data);
+    }
+
+    useEffect(() => {
+        getSurveyData();
+      }, []);
+
     return (
         <div className="Details">
             <div className="Question-header">
-                <h1 className="title">Encuesta: FSANCHEZ-IDF2-E3</h1>      
-                <h3 className="date">Fecha: 13/09/2022</h3>
-                <h3 className="location">Ubicación: 18.805649, -99.221640</h3>
+                <h1 className="title">Encuesta: {surveyData.familyLastName + '-' + surveyData.idFamily + '-' + surveyData.idSurvey}</h1>      
+                <h3 className="date">Fecha: {surveyData.date_}</h3>
+                <h3 className="location">Ubicación: {surveyData.latitude+ ", " + surveyData.longitude}</h3>
             </div>
 
             {/* <LinkButton to="/bamx/encuestas/registros" text="Regresar" /> */}
