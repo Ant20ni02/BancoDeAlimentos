79948116cebece2e9a459d7094a5cd5e46514c9c
ant20ni02 <antoniogarcort@hotmail.com>
2022-10-14 16:12:00 -0500

    Base Inserts of DataBase

--- DataBase/InsertsDB.sql
+++ DataBase/InsertsDB.sql
@@ -1,40 +1,63 @@
 use BancoDeAlimentos;
 
+-- Familia
+
 INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
-VALUES ("E01", 4, "Gardida Santos", 3);
+VALUES ("F01", 4, "Gardida Santos", 13);
 INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
-VALUES ("E02", 3, "Jiménez García", 2);
+VALUES ("F02", 3, "Jiménez García", 2);
 INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
-VALUES ("E03", 3, "Xatruch Adame", 13);
+VALUES ("F03", 3, "Xatruch Adame", 13);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F04", 4, "Hernández", 3);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F05", 3, "Martínez", 3);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F06", 3, "López", 0);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F07", 4, "González", 0);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F08", 3, "Pérez", 9);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F09", 3, "Rodríguez", 13);
+INSERT INTO Family(idFamily, familyMembers, familyLastName, pregnancy)
+VALUES ("F10", 4, "Sánchez", 7);
+
+-- Encuesta
 
 insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E01",10.25,0.55,"27-09-2022");
+Values (1, "F01",18.615576,-99.180106,"27-09-2022");
 insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E01",11.25,1.55,"28-09-2022");
+Values (1, "F02",18.609638,-99.179805,"28-09-2022");
 insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E03",12.25,2.55,"29-09-2022");
+Values (1, "F03",19.035095,-99.268125,"29-09-2022");
 insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E02",13.25,3.55,"30-09-2022");
+Values (1, "F04",18.952639,-99.190534,"30-09-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F05",19.220393,-99.105728,"01-10-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F06",19.409446,-99.152935,"02-10-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F07",19.477432,-99.188297,"03-10-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F08",19.414627,-99.019382,"04-10-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F09",22.252007,-99.670640,"05-10-2022");
+insert into Survey(idUser,idFamily, latitude, longitude, date_) 
+Values (1, "F10",24.047571,-98.900332,"06-10-2022");
 
-insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E03",15.25,4.55,"01-10-2022");
-insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E02",16.25,4.55,"02-10-2022");
-insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E01",17.25,5.55,"03-10-2022");
-insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E03",18.25,6.55,"04-10-2022");
-insert into Survey(idUser,idFamily, latitude, longitude, date_) 
-Values (1, "E02",19.25,7.55,"11-10-2022");
-
 insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
-Values ("E01", 1, "Diabetes");
+Values ("F02", 1, "Diabetes");
 insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
-Values ("E01", 1, "Obesidad");
+Values ("F02", 1, "Diabetes");
 insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
-Values ("E03", 2, "Diabetes");
+Values ("F04", 1, "Diabetes");
 insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
-Values ("E02", 1, "Hipertensión");
+Values ("F05", 2, "Hipertensión");
+insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
+Values ("F07", 1, "Hipertensión");
+insert into MedicalCondition(idFamily, medicalConditionNumber, medicalConditionName)
+Values ("F09", 1, "Obesidad");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(1, 1, "4");
@@ -49,18 +72,20 @@ values(1, 5, "4");
 insert into Question(idQuestion, idSurvey, answer)
 values(1, 6, "5");
 insert into Question(idQuestion, idSurvey, answer)
-values(1, 7, "5");
+values(1, 7, "3");
 insert into Question(idQuestion, idSurvey, answer)
 values(1, 8, "4");
 insert into Question(idQuestion, idSurvey, answer)
 values(1, 9, "4");
+insert into Question(idQuestion, idSurvey, answer)
+values(1, 10, "6");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 1, "a01_b01_c01_d02_e02");
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 2, "b01_c01_d02");
 insert into Question(idQuestion, idSurvey, answer)
-values(2, 3, "a01_d02");
+values(2, 3, "a05_d02");
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 4, "c01_d01_e02");
 insert into Question(idQuestion, idSurvey, answer)
@@ -68,11 +93,13 @@ values(2, 5, "e02");
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 6, "d02");
 insert into Question(idQuestion, idSurvey, answer)
-values(2, 7, "a02_d02");
+values(2, 7, "a03_b03_d02");
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 8, "c02_d01_e02");
 insert into Question(idQuestion, idSurvey, answer)
 values(2, 9, "c03_d01");
+insert into Question(idQuestion, idSurvey, answer)
+values(2, 10, "a01_c03_d02");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(5, 1, "Ninguno");
@@ -92,6 +119,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(5, 8, "CAL-C-TOSE");
 insert into Question(idQuestion, idSurvey, answer)
 values(5, 9, "GLUCERNA");
+insert into Question(idQuestion, idSurvey, answer)
+values(5, 10, "Ninguno");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(6, 1, "Sí");
@@ -111,6 +140,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(6, 8, "No");
 insert into Question(idQuestion, idSurvey, answer)
 values(6, 9, "No");
+insert into Question(idQuestion, idSurvey, answer)
+values(6, 10, "No");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(7, 1, "De 1 a 2 comidas");
@@ -130,6 +161,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(7, 8, "De 1 a 2 comidas");
 insert into Question(idQuestion, idSurvey, answer)
 values(7, 9, "De 3 a más comidas");
+insert into Question(idQuestion, idSurvey, answer)
+values(7, 10, "De 1 a 2 comidas");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(8, 1, "No");
@@ -149,6 +182,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(8, 8, "No");
 insert into Question(idQuestion, idSurvey, answer)
 values(8, 9, "No");
+insert into Question(idQuestion, idSurvey, answer)
+values(8, 10, "No");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(9, 1, "Menos de $5,255 MXN");
@@ -168,6 +203,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(9, 8, "Menos de $5,255 MXN");
 insert into Question(idQuestion, idSurvey, answer)
 values(9, 9, "Menos de $5,255 MXN");
+insert into Question(idQuestion, idSurvey, answer)
+values(9, 10, "Menos de $5,255 MXN");
 
 insert into Question(idQuestion, idSurvey, answer)
 values(10, 1, "20% aproximadamente");
@@ -187,6 +224,8 @@ insert into Question(idQuestion, idSurvey, answer)
 values(10, 8, "35% aproximadamente");
 insert into Question(idQuestion, idSurvey, answer)
 values(10, 9, "50% aproximadamente");
+insert into Question(idQuestion, idSurvey, answer)
+values(10, 10, "20% aproximadamente");
 
 -- "a_01_a_4000"
 Select * from User_;
