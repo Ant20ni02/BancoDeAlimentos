c8b0fabbb0d80c5182669414112e6df2ac8c9625
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-10-18 18:28:42 -0500

    MultiPage fixed for table and added to volunteers page
    Custom hook refactor and rename to MultiStep

--- web-app/src/pages/Volunteers/Volunteers.js
+++ web-app/src/pages/Volunteers/Volunteers.js
@@ -1,12 +1,19 @@
 import TextHeader from '../../components/TextHeader';
 import { useEffect, useState } from "react";
 import '../../styles/Volunteers.css';
-import ContainerVolunteersView from './ContainerVolunteersView';
 import url from '../../config/API';
+import styles from '../../styles/Tooltip.module.css';
+import useMultiStep from "../../custom-hooks/MultiPage";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft, faArrowRight, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
+import VolunteerCardApproved from '../../components/VolunteerCardApproved';
+import VolunteerCardRequest from '../../components/VolunteerCardRequest';
 
 function Volunteers() {
+	const volunteersPerPage = 5;
+	
 	const [value, setValue] = useState('approved');
-	const [user, setUser] = useState({
+	/*const [user, setUser] = useState({
         age: 0,
         email: "No hay datos",
         firstName: "No hay datos",
@@ -16,6 +23,25 @@ function Volunteers() {
         password_: "No hay datos",
         phoneNumber: "No hay datos",
         sex: "N/A",
+    });*/
+
+	const [user, setUser] = useState({
+		data: [{
+			age: 0,
+			email: "No hay datos",
+			firstName: "No hay datos",
+			idUser: 0,
+			isActive: 0,
+			lastName: "No hay datos",
+			password_: "No hay datos",
+			phoneNumber: "No hay datos",
+			sex: "N/A",
+		},],
+	})
+
+	const { maxPage, page, isFirstStep, isLastStep, next, previous, reset, goTo, pageValues } = useMultiStep({
+        values: user.data,
+        pageSize: volunteersPerPage,
     });
 
 	const handleChange = (event) => {
@@ -28,7 +54,6 @@ function Volunteers() {
 			getInactive();
 		}
 		console.log(user);
-
 	};
 
 	const getActive = async (e) => {
@@ -36,7 +61,7 @@ function Volunteers() {
         const response = await fetch(url+`getActiveVolunteers`,{method: 'GET',
                                 headers: {'x-access-token' : localStorage.getItem('token')} });
         const data = await response.json();
-        setUser(data);
+        setUser({data});
 		console.log(data);
     }
 
@@ -45,7 +70,7 @@ function Volunteers() {
         const response = await fetch(url+`getInactiveVolunteers`,{method: 'GET',
                                 headers: {'x-access-token' : localStorage.getItem('token')} });
         const data = await response.json();
-        setUser(data);
+        setUser({data});
 		console.log(data);
     }
 
@@ -64,7 +89,50 @@ function Volunteers() {
 				</select>
 			</div>
 
-			<ContainerVolunteersView value={value} user={user}/>
+			<div className="Volunteers">
+				{value === "approved" && Object.entries(pageValues).map(([key, { firstName, lastName, email, age, sex, phoneNumber, idUser}]) => (
+                    <VolunteerCardApproved volunteer={{firstName: firstName, lastName: lastName, email: email, age: age, sex: sex, phoneNumber: phoneNumber, idUser: idUser}}/>
+                ))}
+                {value === "request" && Object.entries(pageValues).map(([key, { firstName, lastName, email, age, sex, phoneNumber, idUser}]) => (
+                    <VolunteerCardRequest volunteer={{firstName: firstName, lastName: lastName, email: email, age: age, sex: sex, phoneNumber: phoneNumber, idUser: idUser}}/>
+                ))}
+			</div>
+
+			{/* {value === 'approved' ? (
+				<div className='Volunteers'>
+					{pageValues.map((volunteer) => (
+						<VolunteerCardApproved volunteer={volunteer} />
+					))}
+				</div>
+			) : (
+				<div className='Volunteers'>
+					{pageValues.map((volunteer) => (
+						<VolunteerCardRequest volunteer={volunteer} />
+					))}
+				</div>
+			)} */}
+
+			{
+				/* if array is less than zero don't appear buttons  */
+				user.data.length > 0 && (
+					<div className='buttons'>
+						{ !isFirstStep  && 
+							<button onClick={previous} className={styles.stepBtn} data-tooltip="Anterior"><FontAwesomeIcon icon={faArrowLeft} /></button>
+						}
+
+						{ !isLastStep  && 
+							<button onClick={next} className={styles.stepBtn} data-tooltip="Siguiente"><FontAwesomeIcon icon={faArrowRight} /></button>
+						}
+
+						{ !isFirstStep  && 
+							<button onClick={reset} className={styles.stepBtn} data-tooltip="Reiniciar"><FontAwesomeIcon icon={faRotateLeft} /></button>
+						}
+
+						<input type="number" min="1" max={maxPage} value={page + 1} onChange={e => goTo(e.target.value - 1)} />
+					</div>
+				)
+			}
+			
     	</>
   	);
 }
--- web-app/src/pages/survey/Records/Records.js
+++ web-app/src/pages/survey/Records/Records.js
@@ -1,58 +1,68 @@
-import React, { useEffect, useState } from 'react'
-import TextHeader from '../../../components/TextHeader';
-import Table from '../../../components/Table';
-import url from '../../../config/API';
-import '../../../styles/Records.css';
-import styles from '../../../styles/Tooltip.module.css';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight, faRotateLeft } from '@fortawesome/free-solid-svg-icons';
-import useMultiStep from '../../../custom-hooks/MultiStep';
+import "../../../styles/Records.css";
+import React, { useEffect, useState } from "react";
+import Table from "../../../components/Table";
+import TextHeader from "../../../components/TextHeader";
+import styles from "../../../styles/Tooltip.module.css";
+import url from "../../../config/API";
+import useMultiStep from "../../../custom-hooks/MultiPage";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight, faRotateLeft } from "@fortawesome/free-solid-svg-icons";
 
 function Records() {
-    const recordsPerPage = 10
-    const [surveys, setSurveys] = useState({data: []});
-    const {currentStepIndex, step, steps, isFirstStep, isLastStep, next, previous, reset, goTo, } = useMultiStep([
-        <Table data={surveys} />,
-        <Table data={surveys} />,
-        <Table data={surveys} />
-    ]);
-    {/* <Table surveys={surveys} />,
-    <TextHeader text="Step 2" /> */}
-    /*surveys.slice((currentStepIndex+1) * recordsPerPage - recordsPerPage, (currentStepIndex+1) * recordsPerPage - 1)*/
-    useEffect(()=>{
-        getSurveys();
-        }, []);
+	const recordsPerPage = 5;
+	const [surveys, setSurveys] = useState({ data: [] });
+	const { maxPage, page, isFirstStep, isLastStep, next, previous, reset, goTo, pageValues } = useMultiStep({
+		values: surveys.data,
+		pageSize: recordsPerPage,
+	});
+	
+	useEffect(() => {
+		getSurveys();
+	}, []);
+	
+	const getSurveys = async (e) => {
+		const response = await fetch(url + "getFamilyTables", {
+			method: "GET",
+			headers: { "x-access-token": localStorage.getItem("token") },
+		});
+		const data = await response.json();
+		setSurveys({ data });
+	};
+	
+	return (
+		<>
+			<TextHeader text="Registros" />
+      		<div className="Records">
+        		<Table data={pageValues} />
+			</div>
+			
+			<div className="steps">
+				{page + 1} / {maxPage}
+      		</div>
+			
+			<div className="buttons">
+				{ !isFirstStep &&
+					<button onClick={previous} className={styles.stepBtn} data-tooltip="Anterior">
+						<FontAwesomeIcon /* className="table-icon" */ icon={faArrowLeft} />
+					</button>
+				}
 
-    const getSurveys = async (e) => {
-    
-        const response = await fetch(url+'getFamilyTables',{method: 'GET',
-                                headers: {'x-access-token' : localStorage.getItem('token')} });
-        const data = await response.json();
-        setSurveys(data);
-    }
-    return (
-        <>
-            <TextHeader text="Registros" />
-            <div className='Records'>
-                { !(surveys.length === undefined) ?
-                React.cloneElement(step, {newSurveys: surveys.slice((currentStepIndex+1)*5-5, (currentStepIndex+1)*5)}):[]}
-            </div>
-            <div className='steps'> {currentStepIndex + 1} / {steps.length} </div>
-            <div className='buttons'>
-                {!isFirstStep && <button onClick={previous} className={styles.stepBtn} data-tooltip="Anterior"><FontAwesomeIcon /* className="table-icon" */ icon={faArrowLeft} />{/* Anterior */}</button>}
-                {/* <button onClick={previous} disabled={currentStepIndex === 0}>Previous</button> */}
+				{ !isLastStep &&
+					<button onClick={next} className={styles.stepBtn} data-tooltip="Siguiente">
+						<FontAwesomeIcon /* className="table-icon" */ icon={faArrowRight} />
+					</button>		
+				}
 
-                {!isLastStep && <button onClick={next} className={styles.stepBtn} data-tooltip="Siguiente"><FontAwesomeIcon /* className="table-icon" */ icon={faArrowRight} />{/* Siguiente */}</button>}
-                {/* <button onClick={next} disabled={currentStepIndex === steps.length - 1}>Next</button> */}
-                
-                {!isFirstStep && <button onClick={reset} className={styles.stepBtn} data-tooltip="Reiniciar"><FontAwesomeIcon /* className="table-icon" */ icon={faRotateLeft} />{/* Volver al principio */}</button>}
-                {/* <button onClick={reset}>Reset</button> */}
-
-                {/* <button onClick={() => goTo(1)}>Ir a</button> */}
-                <input type="number" min="1" max={steps.length} value={currentStepIndex + 1} onChange={e => goTo(e.target.value - 1)} />
-            </div>
-        </>
-    );
+				{ !isFirstStep &&
+					<button onClick={reset} className={styles.stepBtn} data-tooltip="Reiniciar">
+						<FontAwesomeIcon /* className="table-icon" */ icon={faRotateLeft} />
+					</button>
+				}
+				
+				<input type="number" min="1" max={maxPage} value={page + 1} onChange={(e) => goTo(e.target.value - 1)}/>
+			</div>
+		</>
+	);
 }
 
 export default Records;
