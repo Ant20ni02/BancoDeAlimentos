3d51e0f81b9191c4e4fb70c1a79c75359d3c9a17
ant20ni02 <antoniogarcort@hotmail.com>
2022-10-21 16:20:13 -0500

    New API

--- web-app/src/config/API.js
+++ web-app/src/config/API.js
@@ -1,3 +1,3 @@
-const url = 'http://Bamxapi3-env.eba-j9sdi2k3.us-east-1.elasticbeanstalk.com/';
+const url = 'http://bamxrestapi-env.eba-zn7ggggu.us-east-1.elasticbeanstalk.com/';
 //const url = 'http://localhost:4000/'
 export default url;
