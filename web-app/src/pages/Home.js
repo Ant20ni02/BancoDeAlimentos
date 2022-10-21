8675a5a23c824829ea936e52808b73b3bb6c5919
Elviro Dominguez Soriano <elvirodominguezsoriano@outlook.com>
2022-10-20 20:05:56 -0500

    Home updated
    Dashboard cards added

--- web-app/src/pages/Home.js
+++ web-app/src/pages/Home.js
@@ -1,7 +1,9 @@
 import TextHeader from '../components/TextHeader';
-import React, {useEffect, useState} from 'react';
-import {useHistory} from 'react-router-dom';
 import useTabTitle from '../custom-hooks/useTabTitle';
+import DasboardCard from '../components/DashboardCard';
+import family from '../images/icons/family.png';
+import volunteers from '../images/icons/volunteers.png';
+import home from '../styles/Home.module.css';
 
 function Home() {
     useTabTitle("BAMX - Inicio");
@@ -9,9 +11,11 @@ function Home() {
     return (
         <>
             <TextHeader text="Página principal" />
-            <div>
+            <div className={home.container}>
                 <h1>¡Bienvenido a BAMX!</h1>
-                
+                <DasboardCard img={volunteers} title="Voluntarios" count="1" text="Este es el número actual de voluntarios" />
+
+                <DasboardCard img={family} title="Familia" count="1" text="Este es el número actual de familias" />
             </div>
         </>
     );
